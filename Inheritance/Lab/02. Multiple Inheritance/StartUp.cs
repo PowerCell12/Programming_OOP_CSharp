882c435f9567b20e8f391c7a57ddf3182492077c
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-12-30 10:26:58 +0200

    Add files via upload

---
+++ Inheritance/Lab/02. Multiple Inheritance/StartUp.cs
@@ -0,0 +1,15 @@
+namespace Farm;
+
+
+public class StartUp{
+
+    static void Main(){
+
+        Puppy puppy = new Puppy();
+        puppy.Eat();
+        puppy.Bark();
+        puppy.Weep();
+    }
+
+
+}
