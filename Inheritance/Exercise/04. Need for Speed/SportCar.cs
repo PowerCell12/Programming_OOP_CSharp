0147c0ebfcee845eb8d7d452629608641b6d014f
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-01-08 19:05:20 +0200

    Add files via upload

---
+++ Inheritance/Exercise/04. Need for Speed/SportCar.cs
@@ -0,0 +1,10 @@
+namespace NeedForSpeed;
+
+public class SportCar: Car{
+
+    public SportCar(int horsePower, double fuel): base(horsePower, fuel){
+    }
+
+    public override double FuelConsumption => 10;
+
+}
