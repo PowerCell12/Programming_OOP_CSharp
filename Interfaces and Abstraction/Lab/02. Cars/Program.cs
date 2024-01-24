84049168b8ca2f1ec6865d7270ab905e593ae5ee
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-01-24 21:27:50 +0200

    Add files via upload

---
+++ Interfaces and Abstraction/Lab/02. Cars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO.Compression;
+using System.Linq.Expressions;
+using System.Xml.Serialization;
+
+namespace Cars;
+
+
+
+
+public class StartUp {
+  static void Main()
+  {
+
+
+
+
+
+
+  }
+}
