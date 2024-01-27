f56e031d2164cda9946730a1905947c5cbc55032
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-01-27 14:02:54 +0200

    Add files via upload

---
+++ Interfaces and Abstraction/Exercise/02. Multiple Implementation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO.Compression;
+using System.Linq.Expressions;
+using System.Xml.Serialization;
+
+namespace PersonInfo;
+
+
+
+
+public class StartUp {
+  static void Main()
+  {
+
+
+
+  }
+}
