81cd0cf5d9bb439fcd6a712652a97613ab60f63b
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-01-28 12:35:59 +0200

    Add files via upload

---
+++ Interfaces and Abstraction/Exercise/04. Border Control/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO.Compression;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Serialization;
+
+namespace PersonRobot;
+
+
+
+
+public class StartUp {
+  static void Main()
+  {
+    List<string> all_Ids = new List<string>();
+    
+
+
+    while(true){
+
+      List<string> person_or_robot = Console.ReadLine().Split().ToList();
+
+      if (person_or_robot[0] == "End"){
+        break;
+      }
+
+      if (person_or_robot.Count == 2){
+        all_Ids.Add(person_or_robot[1]);
+      }
+      else{
+        all_Ids.Add(person_or_robot[2]);
+      }
+
+
+
+    }
+
+    string to_end = Console.ReadLine();
+    List<string> right_ones = new List<string>();
+
+    foreach (var id in all_Ids){
+      if (id.EndsWith(to_end)){
+        right_ones.Add(id);
+      }
+    }
+
+
+    foreach(var thing in right_ones){
+      Console.WriteLine(thing);
+    }
+
+
+  }
+}
