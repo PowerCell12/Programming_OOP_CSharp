25ebd97d9d7c0b4a0686dc386d7867e20c9fc6fb
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-02-03 14:12:35 +0200

    Add files via upload

---
+++ Polymorphism/Lab/02. Animals/Cat.cs
@@ -0,0 +1,15 @@
+namespace Animals;
+
+public class Cat : Animal
+{
+    public Cat(string name, string favouriteFood) : base(name, favouriteFood)
+    {
+    }
+
+    public override string ExplainSelf()
+    {
+        return $"I am {Name} and my fovourite food is {Favoritefod} MEEOW";
+    }
+
+
+}
