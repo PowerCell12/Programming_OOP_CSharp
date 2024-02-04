3118a7963280e2a569621e76345276a9ac179887
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-02-04 13:23:48 +0200

    Add files via upload

---
+++ Polymorphism/Lab/03. Shapes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO.Compression;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Serialization;
+
+
+namespace Shapes;
+
+public class StartUp {
+  static void Main()
+  {
+
+
+
+
+
+
+
+  }
+}
