5844093c320304418e70650d1fe752ae213fdee3
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-02-10 12:25:05 +0200

    Add files via upload

---
+++ Polymorphism/Exercise/03. Raiding/BaseHero.cs
@@ -0,0 +1,14 @@
+public abstract class BaseHero{
+    protected BaseHero(string name, int power)
+    {
+        Name = name;
+        Power = power;
+    }
+
+    public string Name{get; set;}
+
+    public int Power{get; set;}
+
+    public abstract string CastAbility();
+
+}
