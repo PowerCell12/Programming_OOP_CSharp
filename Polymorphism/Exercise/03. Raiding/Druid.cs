b0a973ac28fee55db00ee06a33a096631f400742
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-02-10 12:31:43 +0200

    Add files via upload

---
+++ Polymorphism/Exercise/03. Raiding/Druid.cs
@@ -0,0 +1,13 @@
+public class Druid : BaseHero
+{
+
+    public Druid(string name): base(name, 80){
+
+    }
+
+
+    public override string CastAbility()
+    {
+        return $"Druid - {Name} healed for {Power}";
+    }
+}
