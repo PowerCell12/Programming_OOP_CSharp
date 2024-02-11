64267414155a2ba967901819defc76272942828e
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-02-11 15:08:17 +0200

    Add files via upload

---
+++ Polymorphism/Exercise/03. Raiding/Warrior.cs
@@ -0,0 +1,12 @@
+public class Warrior : BaseHero
+{
+
+    public Warrior(string name): base(name, 100){
+
+    }
+
+    public override string CastAbility()
+    {
+        return $"Warrior - {Name} hit for {Power} damage";
+    }
+}
